f91d7fd9faec0674a0234d9110fe61c6a3f72029
eray orçunus <erayorcunus@gmail.com>
2015-06-17 16:25:45 +0300

    lockscreen: Add option to pass swipe-up-to-unlock (2/2)
    * Option appears on PIN,pattern and password methods
    * User should press back button to see notifications, clock etc.
    * Instantly hides keyguard if Smart Lock has unlocked phone.
    * Updated for Marshmallow
    
    Change-Id: I31256770869b20842c69edb4a7a57b2bad7b3ea7

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -38,6 +38,7 @@ import android.os.UserManager;
 import android.os.storage.StorageManager;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.provider.Settings.Secure;
 import android.security.KeyStore;
 import android.service.trust.TrustAgentService;
 import android.support.v14.preference.SwitchPreference;
@@ -91,6 +92,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_ADVANCED_SECURITY = "advanced_security";
     private static final String KEY_MANAGE_TRUST_AGENTS = "manage_trust_agents";
     private static final String KEY_UNIFICATION = "unification";
+    private static final String KEY_DIRECTLY_SHOW = "directlyshow";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CHANGE_TRUST_AGENT_SETTINGS = 126;
@@ -115,8 +117,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     // These switch preferences need special handling since they're not all stored in Settings.
     private static final String SWITCH_PREFERENCE_KEYS[] = {
-            KEY_SHOW_PASSWORD, KEY_TOGGLE_INSTALL_APPLICATIONS, KEY_UNIFICATION,
-            KEY_VISIBLE_PATTERN_PROFILE
+            KEY_SHOW_PASSWORD, KEY_DIRECTLY_SHOW, KEY_TOGGLE_INSTALL_APPLICATIONS,
+            KEY_UNIFICATION,KEY_VISIBLE_PATTERN_PROFILE
     };
 
     // Only allow one trust agent on the platform.
@@ -136,6 +138,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private SwitchPreference mUnifyProfile;
 
     private SwitchPreference mShowPassword;
+    private SwitchPreference mDirectlyShow;
 
     private KeyStore mKeyStore;
     private RestrictedPreference mResetCredentials;
@@ -296,6 +299,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
         mVisiblePatternProfile =
                 (SwitchPreference) root.findPreference(KEY_VISIBLE_PATTERN_PROFILE);
         mUnifyProfile = (SwitchPreference) root.findPreference(KEY_UNIFICATION);
+
+        // directly show
+        mDirectlyShow = (SwitchPreference) root.findPreference(KEY_DIRECTLY_SHOW);
 
         // Append the rest of the settings
         addPreferencesFromResource(R.xml.security_settings_misc);
@@ -601,6 +607,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
         updateUnificationPreference();
 
+        if (mDirectlyShow != null) {
+            mDirectlyShow.setChecked(mLockPatternUtils.shouldPassToSecurityView(
+                    MY_USER_ID));
+        }
+
         if (mShowPassword != null) {
             mShowPassword.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.TEXT_SHOW_PASSWORD, 1) != 0);
@@ -779,6 +790,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
                     ununifyLocks();
                 }
             }
+        } else if (KEY_DIRECTLY_SHOW.equals(key)) {
+            lockPatternUtils.setPassToSecurityView((Boolean) value, MY_USER_ID);
         } else if (KEY_SHOW_PASSWORD.equals(key)) {
             Settings.System.putInt(getContentResolver(), Settings.System.TEXT_SHOW_PASSWORD,
                     ((Boolean) value) ? 1 : 0);
