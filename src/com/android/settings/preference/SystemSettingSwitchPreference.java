d577af6eb6f38f38de5c18f2d89e0a028ca9d6b7
camcory <camcory1@gmail.com>
2016-08-24 16:59:54 -0400

    Remove checkboxes
    Move switches to android.support.v14.preference.SwitchPreference

--- src/com/android/settings/preference/SystemSettingSwitchPreference.java
+++ src/com/android/settings/preference/SystemSettingSwitchPreference.java
@@ -17,8 +17,8 @@
 package com.android.settings.preference;
 
 import android.content.Context;
-import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
 import android.util.AttributeSet;
 
 public class SystemSettingSwitchPreference extends SwitchPreference {
