b2793162ee10fc40f7a8aea6867e1f27b75f3e8d
xlxfoxxlx <joshfox87@gmail.com>
2016-09-03 11:07:45 -0400

    All better? Revert "location: Enable 3dot menu"
    This reverts commit 902b1e64ac93abf80399e98ead9d63e9111b1779.
    
    Change-Id: I2a6c3427de2fbae7bfafbb63968ebf67779186dc

--- src/com/android/settings/location/LocationSettings.java
+++ src/com/android/settings/location/LocationSettings.java
@@ -145,7 +145,6 @@ public class LocationSettings extends LocationSettingsBase
         mSwitchBar = activity.getSwitchBar();
         mSwitch = mSwitchBar.getSwitch();
         mSwitchBar.show();
-        setHasOptionsMenu(true);
     }
 
     @Override
