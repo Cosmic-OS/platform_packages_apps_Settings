Flash-ROM/packages_apps_Settings

DUI: Initial N checkin
Change-Id: If8d277d124e2844810482a52bfb951549ee8c999

src/com/android/settings/SettingsActivity.java
@@ -131,6 +131,10 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 import com.flash.settings.FlashSettings;
+import com.flash.settings.fragments.FlingSettings;
+import com.flash.settings.fragments.NavbarSettings;
+import com.flash.settings.fragments.SmartbarSettings;
+import com.flash.settings.fragments.PulseSettings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -358,9 +362,13 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             FlashSettings.class.getName(),
-            AmbientSettings.class.getName()
-            DisplayRotation.class.getName()
-            ExpandedDesktopPreferenceFragment.class.getName()
+            AmbientSettings.class.getName(),
+            DisplayRotation.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 

Cosmic-OS/platform_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -138,6 +138,10 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 import com.cosmic.settings.CosmicSettings;
+import com.cosmic.settings.fragments.FlingSettings;
+import com.cosmic.settings.fragments.NavbarSettings;
+import com.cosmic.settings.fragments.SmartbarSettings;
+import com.cosmic.settings.fragments.PulseSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -380,8 +384,12 @@ public class SettingsActivity extends SettingsDrawerActivity
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             CosmicSettings.class.getName(),
-            DisplayRotation.class.getName()
-            ExpandedDesktopPreferenceFragment.class.getName()
+            DisplayRotation.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 