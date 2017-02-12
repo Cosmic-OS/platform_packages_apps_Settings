xlxfoxxlx/platform_packages_apps_Settings-2

AppOps: use custom arrays and strings without summaries
Change-Id: I32c51120bed57d9884b493504a7c478ec73024a9

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_labels_aosip);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_aosip);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_aosip);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());

Cosmic-OS/platform_packages_apps_Settings

Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_labels_cosmic);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_cosmic);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_cosmic);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());