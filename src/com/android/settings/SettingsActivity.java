DirtyUnicorns/android_packages_apps_Settings

Add OmniJaws longpress/tile 'more settings' action
- This is combination of service settings and icon pack because
we want to store the icon pack in a Settings.System store

This is a hidden tile that only accessible via the WeatherTile
- more settings - button

Change-Id: I178ff7eae2e9978b19cda30567b337b07f6a42f7

src/com/android/settings/SettingsActivity.java
@@ -140,6 +140,7 @@ import com.dirtyunicorns.dutweaks.fragments.ExpandedDesktop;
 import com.dirtyunicorns.dutweaks.fragments.FlingSettings;
 import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
 import com.dirtyunicorns.dutweaks.fragments.NavbarSettings;
+import com.dirtyunicorns.dutweaks.fragments.QuickSettings;
 import com.dirtyunicorns.dutweaks.fragments.SmartbarSettings;
 import com.dirtyunicorns.dutweaks.fragments.PulseSettings;
 
@@ -384,7 +385,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             HeadsUpSettings.class.getName(),
-            DataSaverSummary.class.getName()
+            DataSaverSummary.class.getName(),
+            QuickSettings.class.getName()
     };
 
 

Cosmic-OS/platform_packages_apps_Settings

Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -144,6 +144,7 @@ import com.cosmic.settings.fragments.NavbarSettings;
 import com.cosmic.settings.fragments.SmartbarSettings;
 import com.cosmic.settings.fragments.PulseSettings;
 import com.cosmic.settings.fragments.HeadsUpSettings;
+import com.cosmic.settings.fragments.NotificationDrawerSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -397,6 +398,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             PulseSettings.class.getName(),
             HeadsUpSettings.class.getName(),
             RunningServices.class.getName(),
+            NotificationDrawerSettings.class.getName()
     };
 
 
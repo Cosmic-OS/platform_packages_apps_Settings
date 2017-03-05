facd283f46f1a2c284adbee472b7b539cf6c0279
maxwen <max.weninger@gmail.com>
2017-03-05 17:40:54 +0100

    Add OmniJaws longpress/tile 'more settings' action
    - This is combination of service settings and icon pack because
    we want to store the icon pack in a Settings.System store
    
    This is a hidden tile that only accessible via the WeatherTile
    - more settings - button
    
    Change-Id: I178ff7eae2e9978b19cda30567b337b07f6a42f7
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -175,4 +175,5 @@ public class Settings extends SettingsActivity {
     public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
+    public static class QuickSettingsSettingsActivity extends SettingsActivity { /* empty */ }
 }
