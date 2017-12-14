dad9c4e1b566d101b48f92b64e1b20149ee8bb01
Sagar Shah <sagarshah2326@gmail.com>
2017-12-14 18:54:47 +0530

    Adapt VersionPreferenceController according to 8.1
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/android/settings/deviceinfo/CosmicVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/CosmicVersionPreferenceController.java
@@ -18,41 +18,36 @@ package com.android.settings.deviceinfo;
 import android.content.Context;
 import android.os.SystemProperties;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
 import android.text.TextUtils;
 
 import com.android.settings.R;
-import com.android.settings.core.PreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
 
-public class CosmicVersionPreferenceController extends PreferenceController {
+public class CosmicVersionPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
 
-
-    private static final String PROPERTY_COSMIC_VERSION = "ro.cos.version";
-    private static final String KEY_COSMIC_VERSION = "cosmic_version";
+    private static final String PROPERTY_COSMIC_VERSION = "cosmic_version";
+    private static final String KEY_COSMIC_VERSION = "ro.cos.version";
 
     public CosmicVersionPreferenceController(Context context) {
         super(context);
     }
 
     @Override
-    public boolean isAvailable() {
-        return !TextUtils.isEmpty(SystemProperties.get(PROPERTY_COSMIC_VERSION));
-    }
-
-    @Override
-    public void displayPreference(PreferenceScreen screen) {
-        super.displayPreference(screen);
-        final Preference pref = screen.findPreference(KEY_COSMIC_VERSION);
-        if (pref != null) {
-            final String summary = SystemProperties.get(PROPERTY_COSMIC_VERSION,
-                    mContext.getResources().getString(R.string.cosmic_version_default));
-            pref.setSummary(summary);
-        }
-    }
-
-    @Override
     public String getPreferenceKey() {
-        return KEY_COSMIC_VERSION;
+        return PROPERTY_COSMIC_VERSION;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !TextUtils.isEmpty(SystemProperties.get(KEY_COSMIC_VERSION));
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        preference.setSummary(SystemProperties.get(KEY_COSMIC_VERSION,
+                mContext.getResources().getString(R.string.device_info_default)));
     }
 }
-
