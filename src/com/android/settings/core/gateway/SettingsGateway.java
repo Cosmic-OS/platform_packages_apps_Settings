69254841063319f84dc897044116d61f9230f3e5
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-20 15:32:25 +0000

    Settings: Nuke Now Playing [2/2]
    - At least until we find a free-for-community recognition service
    
    Change-Id: Ie99c9fd57995a64c6381c9629414eab8b31f1fc6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,7 +137,6 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
-import com.android.settings.custom.ambient.history.AmbientPlayHistory;
 import com.android.settings.livedisplay.LiveDisplaySettings;
 
 import com.cosmic.settings.CosmicSettings;
